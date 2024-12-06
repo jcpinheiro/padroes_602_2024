5041cf77bca668238776bcde8ba4c77389637594
João Carlos <jcpinheiro@ifma.edu.br>
2024-12-06 07:27:13 -0300

    finalização do roteiro 02

---
+++ src/main/java/edu/ifma/dcomp/roteiro01_revisitandoOO/GerenciadorDeDividas.java
@@ -0,0 +1,10 @@
+package edu.ifma.dcomp.roteiro01_revisitandoOO;
+
+public class GerenciadorDeDividas {
+  public void efetuaPagamento(Divida divida, double valor) {
+    //divida.setValorPago(divida.getValorPago() + valor);
+    //divida.paga(valor );
+  }
+ 
+  // outros métodos
+}
---
+++ src/main/java/edu/ifma/dcomp/roteiro02_coesao/CNPJ.java
@@ -0,0 +1,40 @@
+package edu.ifma.dcomp.roteiro02_coesao;
+
+public class CNPJ {
+
+    private String valor;
+
+    public CNPJ(String valor) {
+        if (ehValido() )
+            this.valor = valor;
+        else
+            throw new IllegalArgumentException("CNPJ Inválido!!");
+    }
+
+    public String getValor() {
+        return valor;
+    }
+
+    public boolean ehValido() {
+        return primeiroDigitoVerificadorDoCnpj() == primeiroDigitoCorretoParaCnpj()
+                && segundoDigitoVerificadorDoCnpj() == segundoDigitoCorretoParaCnpj();
+    }
+
+    private boolean segundoDigitoCorretoParaCnpj() {
+        return true;
+    }
+
+    private boolean segundoDigitoVerificadorDoCnpj() {
+        return true;
+    }
+
+    private boolean primeiroDigitoCorretoParaCnpj() {
+        return true;
+    }
+
+    private boolean primeiroDigitoVerificadorDoCnpj() {
+        return true;
+    }
+
+
+}
